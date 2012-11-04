26ff356279acf85c897c8cb911d119330109c4c0
naoya_t <naoya.tozuka@gmail.com>
2012-11-04 20:31:49 +0900

    コメント追加

--- chap5/nnlearn.m
+++ chap5/nnlearn.m
@@ -8,18 +8,23 @@ function [W1, W2, iter] = nnlearn(J, T, Eta, Epsilon)
     for j = 1:M
       X = zeros(M, 1);
       X(j) = 1;
-      A = W1 * [1;X];
+
+      % Y = nn(X, W1, W2) なんだけど
+      % 途中結果をバックプロパゲーションに使うのでここで計算
+      A = W1 * [1; X];
       Z = tanh(A);
-      Y = W2 * [1;Z];
+      Y = W2 * [1; Z];
+
       D2 = Y - T(j);
       DTotal += D2*D2;
-
       W2 -= Eta * (D2 * [1;Z]');
 
       R = 1 - (Z' * Z);
       D1 = R * (W2' * D2); % 4 1
       W1 -= Eta * (D1(2:4) * [1;X]');
     end
+
+    % 誤差の合計がεを下回るようになったら終了
     if DTotal < Epsilon
       break;
     end
